916f8986bb326ffd4d758f362e02fafa1a58634a
Reef Saeed <reefsaeed@hotmail.com>
2025-08-31 16:41:41 +0000

    Update

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle of a given size
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+int i, j, k;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 1; i <= size; i++)
+{
+/* Print spaces */
+for (j = i; j < size; j++)
+{
+_putchar(' ');
+}       
+/* Print hashes */
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}       
+_putchar('\n');
+}
+}
