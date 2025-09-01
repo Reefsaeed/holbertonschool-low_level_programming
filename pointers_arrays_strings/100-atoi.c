07b8f4caafec67e3e8c83bb263cd8633c90c8212
Reef Saeed <reefsaeed@hotmail.com>
2025-09-01 16:38:18 +0000

    Update

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string to convert
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+int started = 0;
+while (*s != '\0')
+{
+if (*s == '-')
+{
+sign *= -1;
+}
+else if (*s == '+')
+{
+/* Do nothing, sign remains the same */
+}
+else if (*s >= '0' && *s <= '9')
+{
+started = 1;
+result = result * 10 + (*s - '0');
+}
+else if (started)
+{
+/* If we already started reading digits and encounter non-digit, stop */
+break;
+}
+s++;
+}
+return (result * sign);
+}
